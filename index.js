24cffba88f316a2cfb4b5ed377d586217e316de1
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-18 18:24:42 +0200

    Display respective page dependent on url

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const http = require("node:http");
 const fs = require("fs");
 
+const htmlPaths = ["", "contact-me", "about"];
+
 const server = http.createServer((req, res) => {
-  fs.readFile("index.html", "utf8", (err, data) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  const currentUrl = req.url.slice(1);
+  const currentPath = htmlPaths.find((path) => path === currentUrl);
+  let fileName = "index";
+
+  if (currentPath === undefined) {
+    fileName = "404";
+  } else if (currentUrl !== "") {
+    fileName = currentPath;
+  }
+
+  fs.readFile(`${fileName}.html`, "utf8", (err, data) => {
+    res.writeHead(fileName === "404" ? 404 : 200, {
+      "Content-Type": "text/html",
+    });
     res.write(data);
-    return res.end();
+    res.end();
   });
 });
 
