1fb5459ba1a9cd83e57c2f34918f8667eb2d634c
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-18 16:47:14 +0200

    Display current url path

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(req.url);
+  res.end();
+});
+
+server.listen(8080);
