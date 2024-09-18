702b198715614ff55fc92a3d3916ff98e2ba2112
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-18 17:20:50 +0200

    Display contents of index.html

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http = require("node:http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(req.url);
-  res.end();
+  fs.readFile("index.html", "utf8", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
 });
 
 server.listen(8080);
