f694c39b75ad6697afd8d28f20429940dcb5277f
chryschi <121822197+chryschi@users.noreply.github.com>
2024-09-20 19:36:24 +0200

    Refactor code using EJS

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,12 @@
 const { Router } = require("express");
-const path = require("node:path");
 
 const indexRouter = Router();
-const options = { root: path.join(__dirname, "../") };
 
-indexRouter.get("/", (req, res) => res.send("Hello World"));
-indexRouter.get("/contact-me", (req, res) =>
-  res.sendFile("./contact-me.html", options)
+indexRouter.get("/", (req, res) => res.render("index"));
+indexRouter.get("/contact-me", (req, res) => res.render("contact-me"));
+indexRouter.get("/about", (req, res) =>
+  res.render("about", { message: "This is the about page!" }),
 );
-indexRouter.get("/about", (req, res) => res.sendFile("./about.html", options));
-indexRouter.get("*", (req, res) =>
-  res.status(404).sendFile("./404.html", options)
-);
+indexRouter.get("*", (req, res) => res.status(404).render("404"));
 
 module.exports = indexRouter;
